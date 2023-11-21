69989d50432ba0512a31e2c488c370db0ae47b81
Antonio Gonzalez <antonio@MacBook-Pro-de-Antonio.local>
2023-11-21 11:11:43 -0600

    Installing framework , documentation and setup database

--- colegio/settings.py
+++ colegio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
     'users'
 ]
 
@@ -76,8 +78,10 @@ WSGI_APPLICATION = 'colegio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "OPTIONS": {
+            "read_default_file": "../config.cnf",
+        },
     }
 }
 
--- colegio/urls.py
+++ colegio/urls.py
@@ -15,7 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+
+    openapi.Info(
+
+        title="Colegio API",
+        default_version='v1',
+        description="This is the API documentation.",
+        terms_of_service="",
+        contact=openapi.Contact( email="antonioglez1801@gmail.com" ),
+        license=openapi.License( name="BSD License" )
+
+   ),
+   public=True,
+   #permission_classes=( permissions.AllowAny, )
+
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path( 'admin/', admin.site.urls),
+    path( 'docs/', schema_view.with_ui( 'swagger', cache_timeout=0 ), name='schema-swagger-ui' ),
+    path( 'redocs/', schema_view.with_ui( 'redoc', cache_timeout=0), name='schema-redoc' )
+
 ]
